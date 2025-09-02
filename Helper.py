e727f510b36e68f24b707be4dbf9803f831ef4a9
vikash jha <jhavikashkumar716@gmail.com>
2025-09-02 12:05:07 +0530

    Initial commit: WhatsApp Chat Analyzer

---
+++ Helper.py
@@ -0,0 +1,139 @@
+from urlextract import URLExtract
+extract = URLExtract()
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+import emoji
+
+def fetch_stats(selected_user, df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    #Fetch number of messages
+    num_messages = df.shape[0]
+    
+    #Fetch number of words
+    words =[]
+    for message in df['message']:
+        words.extend(message.split())
+
+    #fetch number of media messages
+    num_media_messages = df[df['message'].str.contains('<Media omitted>', na=False)].shape[0]
+
+    #fetch nuber of links shred
+    # pip install urlextract
+
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+
+
+    return num_messages, len(words), num_media_messages, len(links)
+
+
+#Find Most busy user in the group
+def most_busy_user(df):
+    x = df['user'].value_counts().head()
+
+    df =round((df['user'].value_counts()/df.shape[0])*100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'}
+    )
+    return x, df
+
+#Create WorlCloud
+
+def create_wordcloud(selected_user, df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+
+    return df_wc
+ 
+
+#most common words
+def most_common_words(selected_user, df):
+
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    temp = df[df['user'] != 'group_notification'] 
+    temp = temp[~temp['message'].str.contains('<Media omitted>', na=False)]
+
+    words =[]
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
+
+#emoji analysis
+def emoji_helper(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    emojis = []
+    for message in df['message']:
+        emojis.extend([c for c in message if c in emoji.EMOJI_DATA])
+
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
+
+    return emoji_df
+
+# Monthly Timeline
+
+def monthly_timeline(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+
+    time =[]
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i]+"-"+str(timeline['year'][i]))
+
+    timeline['time'] = time
+
+    return timeline
+
+# Daily Timeline
+def daily_timeline(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    daily_timeline = df.groupby('only_date').count()['message'].reset_index()
+
+    return daily_timeline
+
+#  Activity map
+def week_activity_map(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    return df['day_name'].value_counts()
+
+# Month Activity Map
+def month_activity_map(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    return df['month'].value_counts()
+
+# Activity heatmap
+def activit_heatmap(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] ==selected_user]
+
+    user_heatmap = df.pivot_table(index='day_name', columns='period', values='message', aggfunc='count').fillna(0)
+
+    return user_heatmap
